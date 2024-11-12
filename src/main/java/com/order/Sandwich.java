b5163ab0574b87c6a40048d55781ce498ecd8b43
BryanG2442 <bryangarcia2442@gmail.com>
2024-11-12 11:04:43 -0700

    Repository creation

---
+++ src/main/java/com/order/Sandwich.java
@@ -0,0 +1,42 @@
+package com.order;
+
+import java.util.ArrayList;
+
+public class Sandwich {
+    private boolean isToasted;
+    private String breadType;
+    private ArrayList<String> topppings;
+    enum BreadSize {
+        FOUR_INCH, EIGHT_INCH, TWELVE_INCH
+    }
+    enum BreadType{
+        WHITE, WHEAT, RYE, WRAP
+    }
+
+    enum RegularToppings{
+        LETTUCE, PEPPERS, ONIONS, TOMATOES, JALAPENOS, CUCUMBERS, PICKLES, GUACAMOLE, MUSHROOMS
+    }
+    enum PremiumMeats{
+        STEAK, HAM, SALAMI, ROAST_BEEF, CHICKEN, BACON
+    }
+    enum PremiumCheese{
+        AMERICAN, PROVOLONE, CHEDDAR, SWISS
+    }
+
+
+    public Sandwich(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
